73f8566912881ea82c0e9e6833ce2a506fb9ea53
siripong6653 <72536609+siripong6653@users.noreply.github.com>
2020-10-20 16:00:57 +0700

    Add files via upload

---
+++ src/Gittest.java
@@ -0,0 +1,13 @@
+public class Gittest {
+    public static void main(String[] args) {
+        sayHello();
+    }
+    public static void  sayHello(){
+        System.out.println("Hello");
+
+
+    }
+    public static void sayWhat(){
+        System.out.println("What");
+    }
+}
